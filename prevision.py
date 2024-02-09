bf9643ed5728f9c5fd0bd0ee9ed057808098aad2
Cayo Cesar <96149641+Cayo-Cesar@users.noreply.github.com>
2024-02-09 10:41:04 -0300

    Interface

--- prevision.py
+++ prevision.py
@@ -1,37 +1,44 @@
 # Description: This file contains the API key and the link to get the weather forecast.
+from tkinter import messagebox
 
 import requests
+import tkinter as tk
+from tkinter import *
+
+def previsao():
+    city = city_name.get()
+    link = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=pt_br"
+    requisicao = requests.get(link)
+
+    if requisicao.status_code == 200:
+        requisicao_dic = requisicao.json()
+        descricao = requisicao_dic["weather"][0]["description"]
+        temperatura = requisicao_dic["main"]["temp"] - 273.15
+        sensacao_termica = requisicao_dic["main"]["feels_like"] - 273.15
+        umidade = requisicao_dic["main"]["humidity"]
+        pressao = requisicao_dic["main"]["pressure"]
+        precipitacao = requisicao_dic["rain"]["1h"] if "rain" in requisicao_dic else 0
+        visibilidade = requisicao_dic["visibility"]
+        ultimo_update = requisicao_dic["dt"]
+
+        messagebox.showinfo("Previsão do tempo", f"Cidade: {city}\nTempo: {descricao}\nTemperatura: {temperatura} °C\nSensação térmica: {sensacao_termica} °C\nUmidade: {umidade}%\nPressão: {pressao} hPa\nChuva: {precipitacao} mm\nVisibilidade: {visibilidade} m\nÚltima atualização: {ultimo_update}")
+        #resultado = f"Cidade: {city}\nTempo: {descricao}\nTemperatura: {temperatura} °C\nSensação térmica: {sensacao_termica} °C\nUmidade: {umidade}%\nPressão: {pressao} hPa\nChuva: {precipitacao} mm\nVisibilidade: {visibilidade} m\nÚltima atualização: {ultimo_update}"
+        resultado_label = Label(window, text=resultado, font=("Courier", 12))
+        resultado_label.pack()
 
 API_KEY = "3cb478365812f07bef9bb14b4fd256ce"
 
-lat = -5.11400579902402
-lon = -42.79944966912756
-city_name = "Demerval Lobão"
+window = tk.Tk()
+window.title("Previsão do tempo")
+window.geometry("300x100")
 
-#link = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&lang=pt_br"
-link = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&lang=pt_br"
-requisicao = requests.get(link)
+label = Label(window, text="Previsão do tempo", font=("Courier", 16, "italic"))
+label.pack()
 
-if requisicao.status_code == 200:
-    print("Requisição bem sucedida")
+city_name = Entry(window, font=("Courier", 12))
+city_name.pack()
 
-requisicao_dic = requisicao.json()
-descricao = requisicao_dic["weather"][0]["description"]
-temperatura = requisicao_dic["main"]["temp"] - 273.15
-sensacao_termica = requisicao_dic["main"]["feels_like"] - 273.15
-umidade = requisicao_dic["main"]["humidity"]
-pressao = requisicao_dic["main"]["pressure"]
-precipitacao = requisicao_dic["rain"]["1h"] if "rain" in requisicao_dic else 0
-visibilidade = requisicao_dic["visibility"]
-ultimo_update = requisicao_dic["dt"]
+button = Button(window, text="Buscar", font=("Courier", 12), command=previsao)
+button.pack()
 
-print(f"Cidade: {city_name}")
-print(f"Tempo: {descricao}")
-print(f"Temperatura: {temperatura} °C")
-print(f"Sensação térmica: {sensacao_termica} °C")
-print(f"Umidade: {umidade}%")
-print(f"Pressão: {pressao} hPa")
-print(f"Chuva: {precipitacao} mm")
-print(f"Visibilidade: {visibilidade} m")
-print(f"Última atualização: {ultimo_update}")
-
+window.mainloop()
